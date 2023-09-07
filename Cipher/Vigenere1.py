84317428deb0f32f557bc2b4322502d144ddd584
angelafonso227 <angel.adrianafonso22@gmail.com>
2023-09-07 15:10:35 -0600

    cambios en el virgenisin 1

--- Cipher/Vigenere1.py
+++ Cipher/Vigenere1.py
@@ -1,6 +1,15 @@
-# Abre el archivo en modo lectura
-with open('archivo.txt', 'r') as archivo:
-    # Lee el contenido del archivo y guárdalo en una variable string
+import os
+
+os.chdir('D:\Documentos\ACTIVIDADES TEC\QUINTO SEMESTRE\Ciberseguridad\ParejasCiberseguridad\Cipher')
+
+with open('cipher2.txt', 'r') as archivo:
     contenido = archivo.read()
 
-print(contenido)
+cadenas_de_cuatro = []
+
+for i in range(0, len(contenido), 4):
+    cadena = contenido[i:i+4]
+    cadenas_de_cuatro.append(cadena)
+
+for cadena in cadenas_de_cuatro:
+    print(cadena)
