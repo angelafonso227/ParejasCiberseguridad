43aadac8c189877fe20b5f185d415aa53faa284a
Daniel <a01782985@tec.mx>
2023-09-10 22:44:51 -0600

    código final, más bonito

--- Cipher/pruebaVigenere.py
+++ Cipher/pruebaVigenere.py
@@ -1,35 +1,51 @@
-import os
+#import os
 
-os.chdir('D:\Documentos\ACTIVIDADES TEC\QUINTO SEMESTRE\Ciberseguridad\ParejasCiberseguridad\Cipher')
+#os.chdir('D:\Documentos\ACTIVIDADES TEC\QUINTO SEMESTRE\Ciberseguridad\ParejasCiberseguridad\Cipher')
 
-alfabeto = "abcdefghijklmnopqrstuvwxyz "
+alfabeto = "abcdefghijklmnopqrstuvwxyz " # alfabeto con un espacio 
 
-letter_to_index = dict(zip(alfabeto, range(len(alfabeto))))
-index_to_letter = dict(zip(range(len(alfabeto)), alfabeto))
+# crear diccionario, para mapear el alfabeto (letras e index)
+letra_index = dict(zip(alfabeto, range(len(alfabeto))))
+index_letra = dict(zip(range(len(alfabeto)), alfabeto))
 
-def decrypt(cipher, key):
-    decrypted = ""
-    split_encrypted = [
-        cipher[i : i + len(key)] for i in range(0, len(cipher), len(key))
-    ]
+def descifrar(cifrado, key):
+    resultado = ""
+    separar = [cifrado[i : i + len(key)] for i in range(0, len(cifrado), len(key))] # divide el mensaje en partes iguales a la longitud de la llave
 
-    for each_split in split_encrypted:
+    for cada_espacio in separar:
         i = 0
-        for letter in each_split:
-            number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alfabeto)
-            decrypted += index_to_letter[number]
+        for letra in cada_espacio:
+            num = (letra_index[letra] - letra_index[key[i]]) % len(alfabeto)
+            resultado += index_letra[num] # agrega la letra resuelta al resultado
             i += 1
 
-    return decrypted
+    return resultado
 
+
+def cifrar(text_cifrar, key):
+    res_cifrar = ""
+    separar = [text_cifrar[i : i + len(key)] for i in range(0, len(text_cifrar), len(key))] # divide el mensaje en partes iguales a la longitud de la llave
+
+    for cada_espacio in separar:
+        i = 0
+        for letra in cada_espacio:
+            num = (letra_index[letra] + letra_index[key[i]]) % len(alfabeto)
+            res_cifrar += index_letra[num] # agrega la letra resuelta al resultado
+            i += 1
+
+    return res_cifrar
 
 def main():
     with open("cipher2.txt", 'r') as archivo:
         message = archivo.read()
     key = "hack"
-    decrypted_message = decrypt(message, key)
+    mensajeDescifrado = descifrar(message, key)
+    mensajeCifrado = cifrar(mensajeDescifrado, key)
 
-    print("Mensaje: " + decrypted_message)
-
+    print("Mensaje descifrado:")
+    print(mensajeDescifrado)
+    print("")
+    print("Mensaje cifrado: ")
+    print(mensajeCifrado)
 
 main()
